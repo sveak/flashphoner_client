2851905188a15c765a6a34dc1a99a04fa56feaec
Egor Kuznetsov <kuznetsov@sveak.com>
2023-11-20 16:08:46 +0700

    Disable flash build

--- Gruntfile.js
+++ Gruntfile.js
@@ -231,12 +231,9 @@ module.exports = function(grunt) {
     grunt.registerTask('build', [
         'clean:build',
         'string-replace:version',
-        'flash:release_media_provider',
         'browserify',
         'concat',
         'minify',
-        'flash:release_examples_streaming',
-        'flash:release_examples_chat',
         'jsdoc'
     ]);
     grunt.registerTask('release', [
